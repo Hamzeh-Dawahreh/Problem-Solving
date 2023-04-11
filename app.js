c040504cfbf0b43667d521c07e9b4825592eb77b
Dawahreh <HAMZA.MOHMMAD99@YAHOO.COM>
2023-04-11 12:08:34 +0300

    Updated

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // 1-Fibonacci Series:
 
+//       Using for loop
+
 // function fibonacci(num) {
 //   let arr = [0, 1];
 //   for (let i = 2; i <= num; i++) {
@@ -8,7 +10,19 @@
 //   return arr;
 // }
 
-// console.log(fibonacci(10));
+//            Using Recursion
+// function fibonacci(n) {
+//   if (n <= 1) {
+//     return n;
+//   }
+//   return fibonacci(n - 1) + fibonacci(n - 2);
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(fibonacci(i));
+// }
+
+// console.log(fibonacci(2));
 
 // 2-The sum of Array Elements:
 
@@ -19,6 +33,7 @@
 //     return arr[0] + arraySum(arr.slice(1));
 //   }
 // }
+// //1 + arraySum[2, 3, 4, 5] -  2 + arraySum[3, 4 ,5] - 3 + arraySum[4 ,5] - 4 + arraySum[5] - 5 + arraySum[0]
 
 // console.log(arraySum([1, 2, 3, 4, 5]));
 // console.log(arraySum([10, -5, 7, 2]));
@@ -60,6 +75,7 @@
 // class minValue {
 //   constructor() {
 //     this.minStack = [];
+//     // [20, 10, 5]
 //   }
 
 //   getMin() {
@@ -82,7 +98,7 @@
 
 // 2.Given a queue of integers, reverse the order of the elements in the queue.
 // function reverseQueue(queue) {
-//   const stack = [];
+//   let stack = [];
 //   while (queue.length > 0) {
 //     stack.push(queue.shift());
 //   }
@@ -92,47 +108,47 @@
 //   return queue;
 // }
 
-// const myQueue = [1, 2, 3, 4];
+// let Queue = [1, 2, 3, 4];
 
-// const reversedQueue = reverseQueue(myQueue);
+// const reversedQueue = reverseQueue(Queue);
 
 // console.log("Reversed queue:", reversedQueue);
 
 // 3.Implement a Queue using 2 stacks s1 and s2.
 
-class Queue {
-  constructor() {
-    this.stack1 = [];
-    this.stack2 = [];
-  }
+// class Queue {
+//   constructor() {
+//     this.stack1 = [];
+//     this.stack2 = [];
+//   }
 
-  enqueue(value) {
-    this.stack1.push(value);
-  }
+//   enqueue(value) {
+//     this.stack1.push(value);
+//   }
 
-  dequeue() {
-    if (this.stack2.length === 0) {
-      while (this.stack1.length > 0) {
-        this.stack2.push(this.stack1.pop());
-      }
-    }
+//   dequeue() {
+//     if (this.stack2.length === 0) {
+//       while (this.stack1.length > 0) {
+//         this.stack2.push(this.stack1.pop());
+//       }
+//     }
 
-    return this.stack2.pop();
-  }
-}
-const q = new Queue();
+//     return this.stack2.pop();
+//   }
+// }
+// const q = new Queue();
 
-q.enqueue("apple");
-q.enqueue("banana");
-q.enqueue("cherry");
-console.log(q.dequeue()); // "apple"
-console.log(q.dequeue()); // "banana"
+// q.enqueue("apple");
+// q.enqueue("banana");
+// q.enqueue("cherry");
+// console.log(q.dequeue()); // "apple"
+// console.log(q.dequeue()); // "banana"
 
-q.enqueue("orange");
+// q.enqueue("orange");
 
-console.log(q.dequeue()); // "cherry"
-console.log(q.dequeue()); // "orange"
-console.log(q.dequeue()); // null
+// console.log(q.dequeue()); // "cherry"
+// console.log(q.dequeue()); // "orange"
+// console.log(q.dequeue()); // null
 
 // 4.Create a function that takes a LinkedList and deletes the middle node in it and returns it
 
@@ -164,20 +180,60 @@ console.log(q.dequeue()); // null
 
 // 5.Create a function that takes a LinkedList and reverses it
 
-// function reverseLinkedList(linkedList) {
-//   let previousNode = null;
-//   let currentNode = linkedList.head;
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
-//   while (currentNode) {
-//     let nextNode = currentNode.next;
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
-//     currentNode.next = previousNode;
+  addNode(data) {
+    const newNode = new Node(data);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let lastNode = this.head;
+      while (lastNode.next !== null) {
+        lastNode = lastNode.next;
+      }
+      lastNode.next = newNode;
+    }
+  }
 
-//     previousNode = currentNode;
-//     currentNode = nextNode;
-//   }
+  printList() {
+    let currNode = this.head;
+    while (currNode !== null) {
+      console.log(currNode.data);
+      currNode = currNode.next;
+    }
+    console.log("");
+  }
 
-//   linkedList.head = previousNode;
+  reverse() {
+    let prevNode = null;
+    let currNode = this.head;
+    while (currNode !== null) {
+      let nextNode = currNode.next;
+      currNode.next = prevNode;
+      prevNode = currNode;
+      currNode = nextNode;
+    }
+    this.head = prevNode;
+  }
+}
 
-//   return linkedList;
-// }
+function reverseLinkedList(linkedList) {
+  linkedList.reverse();
+  return linkedList;
+}
+
+const list = new LinkedList();
+list.addNode(1);
+list.addNode(2);
+list.addNode(3);
+list.addNode(4);
